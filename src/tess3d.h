8bbc5a64b3c26c5101bd98ba56cda15e8610933c
Don Bright <hugh.m.bright@gmail.com>
2013-05-06 19:16:34 -0500

    forgot to add tess engine header

---
+++ src/tess3d.h
@@ -0,0 +1,42 @@
+#ifndef TESS3D_H_
+#define TESS3D_H_
+
+#include "cgal.h"
+
+typedef enum tessellation_e {
+	NO_TESSELLATION,
+	CGAL_NEF_STANDARD, // internal to CGAL's Nef->Polyhedron converter
+	CONSTRAINED_DELAUNAY_TRIANGULATION,
+	STRAIGHT_SKELETON
+} Tessellation;
+
+typedef enum tessellater_status_e {
+        TESSELLATER_OK,
+        BODY_NOT_SIMPLE,
+        BODY_NOT_COPLANAR,
+        BODY_ONLY_COLLINEAR,
+        BODY_LACKS_POINTS,
+        HOLE_ONLY_COLLINEAR,
+        HOLE_NOT_SIMPLE,
+        HOLE_OUTSIDE_BODY,
+        HOLE_INSIDE_HOLE,
+        HOLE_LACKS_POINTS,
+        HOLE_NOT_COPLANAR,
+        PROJECTION_2D3D_FAILED,
+} tessellater_status;
+
+/* Tessellate the input polygon (with holes) into one or more output polygons
+   (without holes).
+
+   The first Polygon_3 in the input represents the 'outline' or 'body' contour.
+   If there are subsequent Polygon_3s in the input, they represent 'holes'.
+   The Body and Holes must all be simple, ordinary non-intersecting polygons.
+
+   The output is a sequence of Polygon_3, none of which will have holes.
+*/
+tessellater_status tessellate(
+        std::vector<CGAL_Polygon_3> &input_pgon3d,
+        std::vector<CGAL_Polygon_3> &output_pgons3d,
+        Tessellation tesstype );
+
+#endif
