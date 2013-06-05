502ca11f7430645a9ae0963317ea597057cc2b71
Don Bright <hugh.m.bright@gmail.com>
2013-06-04 19:14:10 -0500

    change char string code to wchar_t string code. add guarded strcat

--- src/winconsole.c
+++ src/winconsole.c
@@ -22,8 +22,13 @@
 
  TODO:
  Work with unicode: http://www.i18nguy.com/unicode/c-unicode.html
+
+ TODO: rewrite to use strsafe.h, replace mingw32 with
+  mingw-w64's 32-bit system in the build
 */
 
+#define _WIN32_WINNT 0x0500
+#include <windows.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,54 +36,73 @@
 #define MAXCMDLEN 64000
 #define BUFFSIZE 42
 
-int main( int argc, char * argv[] )
+void guard_wcscat( wchar_t * s, wchar_t * s2 )
+{
+	if (lstrlenW(s)+lstrlenW(s2)+2 >= MAXCMDLEN) return;
+	wcsncat( s, s2, lstrlenW(s2) );
+}
+
+int wmain( int argc, wchar_t * wargv[] )
 {
 	FILE *cmd_stdout;
-	char cmd[MAXCMDLEN];
-	char buffer[BUFFSIZE];
-	char *fgets_result;
+	wchar_t cmd[MAXCMDLEN];
+	wchar_t buffer[BUFFSIZE];
+	wchar_t *fgets_result;
 	int eof = 0;
 	int pclose_result;
 	int i;
 	int result = 0;
 
-	strcat( cmd, "\0" );
-	strcat( cmd, "openscad.exe" );
+	guard_wcscat( cmd, L"openscad.exe" );
 	for ( i = 1 ; i < argc ; ++i ) {
-		strcat( cmd, " " );
-		strcat( cmd, argv[i] );
+		guard_wcscat( cmd, L" " );
+		guard_wcscat( cmd, wargv[i] );
 	}
-	strcat( cmd, " ");
-	strcat( cmd, " 2>&1"); // capture stderr and stdout
+	guard_wcscat( cmd, L" " );
+	guard_wcscat( cmd, L" 2>&1" ); // capture stderr and stdout
 
-	cmd_stdout = _popen( cmd, "rt" );
+	cmd_stdout = _wpopen( cmd, L"rt" );
 	if ( cmd_stdout == NULL ) {
-		printf( "Error opening _popen for command: %s", cmd );
-		perror( "Error message:" );
+		wprintf( L"Error opening _popen for command: %s", cmd );
+		_wperror( L"Error message:" );
 		return 1;
 	}
 
 	while ( !eof )
 	{
-		fgets_result = fgets( buffer, BUFFSIZE, cmd_stdout );
+		fgets_result = fgetws( buffer, BUFFSIZE, cmd_stdout );
 		if ( fgets_result == NULL ) {
 			if ( ferror( cmd_stdout ) ) {
-				printf("Error reading from stdout of %s\n", cmd);
+				wprintf(L"Error reading from stdout of %s\n", cmd);
 				result = 1;
 			}
 			if ( feof( cmd_stdout ) ) {
 				eof = 1;
 			}
 		} else {
-			fprintf( stdout, "%s", buffer );
+			fputws( buffer, stdout );
 		}
 	}
 
 	pclose_result = _pclose( cmd_stdout );
 	if ( pclose_result < 0 ) {
-		perror("Error while closing stdout for command:");
+		_wperror(L"Error while closing stdout for command:");
 		result = 1;
 	}
 
 	return result;
 }
+
+#if defined( __MINGW32__ ) || defined( __MINGW64__ )
+int main( int argc, char * argv[] )
+{
+	(void)argc;
+	(void)argv;
+	int wargc;
+	wchar_t * wcmdline = GetCommandLineW();
+	wchar_t ** wargv;
+	wargv = CommandLineToArgvW( wcmdline, &wargc );
+	return wmain(wargc, wargv);
+}
+#endif
+
