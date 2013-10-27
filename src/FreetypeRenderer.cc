34a22c20a01e2e0b98d0b51ecfc6241c74ec4331
Torsten Paul <Torsten.Paul@gmx.de>
2013-10-27 02:21:13 +0200

    Add font name lookup (without any caching for now) and remove some debug stuff.

--- src/FreetypeRenderer.cc
+++ src/FreetypeRenderer.cc
@@ -1,23 +1,50 @@
-/* 
- * File:   FreetypeRenderer.cpp
- * Author: tp
- * 
- * Created on October 21, 2013, 10:43 PM
+/*
+ *  OpenSCAD (www.openscad.org)
+ *  Copyright (C) 2009-2011 Clifford Wolf <clifford@clifford.at> and
+ *                          Marius Kintel <marius@kintel.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  As a special exception, you have permission to link this program
+ *  with the CGAL library and distribute executables, as long as you
+ *  follow the requirements of the GNU GPL in regard to all of the
+ *  software in the executable aside from CGAL.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 #include <stdio.h>
 
 #include <iostream>
+#include <boost/locale.hpp>
+#include <boost/filesystem.hpp>
 
+#include "printutils.h"
 #include "FreetypeRenderer.h"
 
 #include FT_GLYPH_H
 #include FT_OUTLINE_H
 
+namespace fs = boost::filesystem;
+
 FreetypeRenderer::FreetypeRenderer()
 {
+	init_ok = false;
 	const FT_Error error = FT_Init_FreeType(&library);
 	if (error) {
-		printf("can't initialize freetype.\n");
+		PRINT("Can't initialize freetype library, text() objects will not be rendered");
+	} else {
+		init_ok = true;
 	}
 }
 
@@ -28,8 +55,6 @@ FreetypeRenderer::~FreetypeRenderer()
 int
 FreetypeRenderer::Outline_MoveToFunc(const FT_Vector *to, void *user)
 {
-	std::cout << "Outline_MoveToFunc: " << to->x << ", " << to->y << std::endl;
-	
 	const DrawingCallback *cb = reinterpret_cast<const DrawingCallback *>(user);
 	
 	cb->move_to(point_t(to->x, to->y, scale));
@@ -39,8 +64,6 @@ FreetypeRenderer::Outline_MoveToFunc(const FT_Vector *to, void *user)
 int
 FreetypeRenderer::Outline_LineToFunc(const FT_Vector *to, void *user)
 {
-	std::cout << "Outline_LineToFunc: " << to->x << ", " << to->y << std::endl;
-
 	const DrawingCallback *cb = reinterpret_cast<const DrawingCallback *>(user);
 	
 	cb->line_to(point_t(to->x, to->y, scale));
@@ -50,8 +73,6 @@ FreetypeRenderer::Outline_LineToFunc(const FT_Vector *to, void *user)
 int
 FreetypeRenderer::Outline_ConicToFunc(const FT_Vector *c1, const FT_Vector *to, void *user)
 {
-	std::cout << "Outline_ConicToFunc: " << to->x << ", " << to->y << std::endl;
-
 	const DrawingCallback *cb = reinterpret_cast<const DrawingCallback *>(user);
 	
 	cb->curve_to(point_t(c1->x, c1->y, scale), point_t(to->x, to->y, scale));
@@ -61,12 +82,46 @@ FreetypeRenderer::Outline_ConicToFunc(const FT_Vector *c1, const FT_Vector *to, 
 int
 FreetypeRenderer::Outline_CubicToFunc(const FT_Vector *c1, const FT_Vector *c2, const FT_Vector *to, void *user)
 {
-	std::cout << "Outline_CubicToFunc: " << to->x << ", " << to->y << std::endl;
-
 	const DrawingCallback *cb = reinterpret_cast<const DrawingCallback *>(user);
 	
 	cb->curve_to(point_t(c1->x, c1->y, scale), point_t(c2->x, c2->y, scale), point_t(to->x, to->y, scale));
 	return 0; 
+}
+
+FT_Face
+FreetypeRenderer::find_face(std::string font) const
+{
+	const char *env_font_path = getenv("OPENSCAD_FONT_PATH");
+	const std::string path = (env_font_path == NULL) ? "/usr/share/fonts/truetype" : env_font_path;
+	
+	return find_face_in_path(path, font);
+}
+
+FT_Face
+FreetypeRenderer::find_face_in_path(std::string path, std::string font) const
+{
+	FT_Error error;
+	
+	if (!fs::is_directory(path)) {
+		PRINTB("Font path '%s' does not exist or is not a directory.", path);
+	}
+
+	for (fs::recursive_directory_iterator it(path);it != fs::recursive_directory_iterator();it++) {
+		fs::directory_entry entry = (*it);
+		if (fs::is_regular(entry.path())) {
+			FT_Face face;
+			error = FT_New_Face(library, entry.path().c_str(), 0, &face);
+			if (error) {
+				continue;
+			}
+			const char *name = FT_Get_Postscript_Name(face);
+			if (font == name) {
+				return face;
+			}
+			FT_Done_Face(face);
+		}
+	}
+	return NULL;
 }
 
 void
@@ -74,22 +129,19 @@ FreetypeRenderer::render(DrawingCallback *callback, std::string text, std::strin
 {
 	FT_Face face;
 	FT_Error error;
-	
-	std::string path = font.empty() ? "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf" : font;
-	error = FT_New_Face(library, path.c_str(), 0, &face);
-	if (error == FT_Err_Unknown_File_Format) {
-		printf("unknown file format\n");
-		return;
-	} else if (error) {
-		printf("error opening font face\n");
+
+	if (!init_ok) {
 		return;
 	}
-
-	printf("num_glyphs: %ld\n", face->num_glyphs);
+	
+	face = find_face(font);
+	if (face == NULL) {
+		return;
+	}
 	
 	error = FT_Set_Char_Size(face, 0, size * scale, 96, 96);
 	if (error) {
-		printf("can't set font size\n");
+		PRINTB("Can't set font size for font %s", font);
 		return;
 	}
 	
@@ -98,36 +150,29 @@ FreetypeRenderer::render(DrawingCallback *callback, std::string text, std::strin
 	double x_offset = 0;
 	callback->set_xoffset(x_offset);
 	FT_UInt prev_glyph_index = 0;
+	
 	for (unsigned int idx = 0;idx < text.length();idx++) {
 		int c = text.at(idx);
 		FT_UInt glyph_index = FT_Get_Char_Index(face, c);
-		printf("glyph_index: %u\n", glyph_index);
 
 		if (use_kerning && (idx > 0)) {
 			FT_Vector delta;
 			FT_Get_Kerning(face, prev_glyph_index, glyph_index, FT_KERNING_DEFAULT, &delta );
-			std::cout << "kerning delta: x = " << delta.x << ", y = " << delta.y << std::endl;
 		}
 		prev_glyph_index = glyph_index;
 
 		error = FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT);
 		if (error) {
-			printf("could not load glyph %u\n", glyph_index);
+			PRINTB("Could not load glyph %u for char '%c'", glyph_index % c);
 			continue;
 		}
 
 		FT_Glyph glyph;
 		error = FT_Get_Glyph(face->glyph, &glyph);
 		if (error) {
-			printf("could not get glyph %u\n", glyph_index);
+			PRINTB("Could not load glyph %u for char '%c'", glyph_index % c);
 			continue;
 		}
-
-		FT_BBox bbox;
-		FT_Glyph_Get_CBox(glyph, FT_GLYPH_BBOX_UNSCALED, &bbox);
-		int width = bbox.xMax - bbox.xMin;
-		int height = bbox.yMax - bbox.yMin;
-		printf("width = %d, height = %d\n", width, height);
 
 		FT_Outline_Funcs funcs;
 		funcs.move_to = Outline_MoveToFunc;
@@ -139,10 +184,8 @@ FreetypeRenderer::render(DrawingCallback *callback, std::string text, std::strin
 
 		FT_Outline outline = reinterpret_cast<FT_OutlineGlyph>(glyph)->outline;
 		FT_Outline_Decompose(&outline, &funcs, callback);
-		callback->stroke();
 
 		double adv  = glyph->advance.x / scale / 64.0 / 16.0;
-		printf("advance.x = %ld, advance.y = %ld, adv = %.2f\n", glyph->advance.x, glyph->advance.y, adv);
 		callback->add_xoffset(adv);
 		
 		FT_Done_Glyph(glyph);
